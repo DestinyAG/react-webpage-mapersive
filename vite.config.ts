7d16313de4853186e429da8997e387c380867bdb
Destiny Agbagbo <dsent16@gmail.com>
2023-08-16 16:21:14 +0100

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:'/vite-deploy-demo/',
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
